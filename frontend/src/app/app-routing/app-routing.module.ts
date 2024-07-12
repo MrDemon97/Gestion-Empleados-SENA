c7bfe4084fd3db292c11202eafcf3881ef4498f6
Jair Coral <jaircoral88@gmail.com>
2024-07-11 21:28:58 -0500

    Corrigiendo error en app routing

--- frontend/src/app/app-routing/app-routing.module.ts
+++ frontend/src/app/app-routing/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { EmpleadosComponent } from '../components/empleados/empleados.component';
 import { CommonModule } from '@angular/common';
 
