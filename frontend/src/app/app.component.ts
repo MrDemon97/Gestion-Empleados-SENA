879de712aa9120cd331aa58a3f138153e2eaeedb
Jair Coral <jaircoral88@gmail.com>
2024-07-23 20:30:39 -0500

    "Updated app component title and logo, removed unnecessary HTML comments, and added CSS styles for column width."

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { EmpleadosComponent } from './components/empleados/empleados.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Gestion de empleados';
 }
