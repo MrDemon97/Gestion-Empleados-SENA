5a87e7d445abbd1e507661a381f81bdb785ba648
Jair Coral <jaircoral88@gmail.com>
2024-07-31 08:40:54 -0500

    Depuracion de código

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,23 +7,24 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
  */
+
+
+
+//USAMOS EL ENFOQUE BASADO EN MODULOS PUES EL TUTORIAL SE REALIZO CON
+// UNA VERSION ANTERIOR DE ANGULAR
+
 import { importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; // Asegúrate de importar esto
 import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { routes } from './app-routing.module';
 
 export const appConfig = {
   providers: [
     importProvidersFrom(
       BrowserModule,
       HttpClientModule,
-      RouterModule.forRoot([
-        { path: '', redirectTo: '/empleados', pathMatch: 'full' },
-        { path: 'empleados', component: EmpleadosComponent },
-        { path: '**', redirectTo: '/empleados' }
-      ])
+      RouterModule.forRoot(routes)
     )
   ]
 };
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +18,6 @@ import { EmpleadosComponent } from './components/empleados/empleados.component';
         AppRoutingModule,
         FormsModule,
         HttpClientModule,
-        HttpClient
     ],
 
     providers: [],
